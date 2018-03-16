bd604903d5c9e2b619e5387e432fd4d52699bdfb
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-16 08:36:19 -0700

    add basic file structure and dependencies.

---
+++ src/fight.js
@@ -0,0 +1,13 @@
+export class Fight {
+  constructor(wizard, necro) {
+    this.wizard = wizard;
+    this.necro = necro;
+    this.turn = true; // if true, wizard turn; if false, necro
+  }
+
+  strike() {
+
+  } // strike
+
+  
+} //Fight
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Wizard } from './wizard.js';
+
+
+$(document).ready(function() {
+
+
+});
